6bb133e35cc518ab4317d5de692591253d59d551
yegireddihemanth <84504741+yegireddihemanth@users.noreply.github.com>
2022-08-24 16:08:05 +0530

    Add files via upload

---
+++ k_meansPlot.py
@@ -0,0 +1,17 @@
+import inline as inline
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+X = np.array([[5,3],
+     [10,15],
+     [15,12],
+     [24,10],
+     [30,45],
+     [85,70],
+     [71,80],
+     [60,78],
+     [55,52],
+     [80,91],])
+x=plt.scatter(X[:,0],X[:,1], label='True Position')
+print(x)
